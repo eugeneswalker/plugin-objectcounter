7bbd9339fc603471834d1628f73a0339aaf21489
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-08-30 13:35:35 -0700

    wrapper.sh: echo debug statements

--- wrapper.sh
+++ wrapper.sh
@@ -2,10 +2,17 @@
 
 export PROFILE_DIR=$(pwd)/profiles
 
+echo Launching tau_python
 tau_python /app/app.py $@
 
+echo Collecting profile files
 mkdir -p $PROFILE_DIR
 mv profile.* $PROFILE_DIR/
+
+echo Creating tarball
 tar czvf profiles.tgz $PROFILE_DIR
 
+echo Uploading tarball
 python3 upload.py --file-path ./profiles.tgz
+
+echo Success!
